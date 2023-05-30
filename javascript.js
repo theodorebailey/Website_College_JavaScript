5b4b1dee110a59e8a2b1c32c0633451dea299332
Theodore Bailey <tawbailey@hotmail.com>
2023-05-30 15:51:57 +0100

    Working project, JavaScript functionality about colour change

--- javascript.js
+++ javascript.js
@@ -6,7 +6,36 @@ tincanDate.addEventListener('click', () => {
 });
 
 function logoSpin() {
+  // select logo ID element
   const logo = document.getElementById('tincanLogo');
+  // set CSS styling property to rotate 360
   logo.style.transform = 'rotate(360deg)';
 }
 
+
+function changeColor () {
+  
+  // Select id element
+  const textElement = document.getElementById("company-about");
+  
+  // create random colour
+  const randomColor = getRandomColor();
+
+  // set random colour to text element
+  textElement.style.color = randomColor;
+
+}
+
+function getRandomColor() {
+  // hexadecimal color code
+  const letters = '0123456789ABCDEF';
+  // create color, instantiated with hash key
+  let color = '#';
+  // create 6 additional random colour numerals
+  for (let i = 0; i < 6; i++) {
+    // add to colour letter array select random 
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  // return hexidecimal value
+  return color;
+}
